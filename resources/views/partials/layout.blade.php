f01c577ee4580fad049b95263835c5c50c0bc783
Javi Santos <javier@3sidedcube.com>
2024-12-23 23:29:42 +0100

    gtag

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -44,6 +44,15 @@
     <meta name="theme-color" content="#ffffff">
     <meta name="color-scheme" content="light">
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MLPR5VRH');</script>
+    <!-- End Google Tag Manager -->
+
+
     <link rel="preconnect" href="https://{{ config('algolia.connections.main.id') }}-dsn.algolia.net" crossorigin />
 
     <link rel="stylesheet" href="https://use.typekit.net/ins2wgm.css">
@@ -79,6 +88,11 @@
     class="w-full h-full font-sans antialiased text-gray-900 language-php"
     data-instant-intensity="0"
 >
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLPR5VRH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 @yield('content')
 
