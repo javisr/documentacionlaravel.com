280fe809d049f961a513cc4b0a4165a09f092fd3
Javi Santos <javier@3sidedcube.com>
2025-06-03 17:08:57 +0200

    remove gtag and plausible

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -44,15 +44,6 @@
     <meta name="theme-color" content="#ffffff">
     <meta name="color-scheme" content="light">
 
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MLPR5VRH');</script>
-    <!-- End Google Tag Manager -->
-
-
     <link rel="preconnect" href="https://{{ config('algolia.connections.main.id') }}-dsn.algolia.net" crossorigin />
 
     <link rel="stylesheet" href="https://use.typekit.net/ins2wgm.css">
@@ -61,11 +52,6 @@
         'resources/js/app.js',
         ...request()->is('docs/*') ? ['resources/js/docs.js'] : [],
     ])
-
-    @production
-    <!-- Plausible -->
-    <script defer data-domain="documentacionlaravel.com" src="https://stats.documentacionlaravel.com/js/script.outbound-links.js"></script>
-    @endproduction
 
     @php
         $routesThatAreAlwaysLightMode = collect([
@@ -88,11 +74,6 @@
     class="w-full h-full font-sans antialiased text-gray-900 language-php"
     data-instant-intensity="0"
 >
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLPR5VRH"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 @yield('content')
 
